54c8b9f41cb5206119ae415165abaa48d7064305
Shreya Kumari <157014499+shreya-arya00@users.noreply.github.com>
2024-06-27 17:59:25 +0530

    Add files via upload

---
+++ your_application/src/YourApplication.java
@@ -0,0 +1,11 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class YourApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(YourApplication.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Application started.");
+        LOGGER.info("Application finished.");
+    }
+}
